de5be6d0e16d2bbe9a33cb7186800da6bed62b6c
W. Cho <wcho.dev@gmail.com>
2023-12-08 02:15:25 +0900

    make test watch command

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  // default settings from ts-jest
   preset: 'ts-jest',
   testEnvironment: 'node',
+
+  // custum settings
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
