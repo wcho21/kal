581627dc65fda5ba2ba142141ce896524df2deac
W. Cho <wcho.dev@gmail.com>
2023-12-21 15:30:02 +0900

    add call syntax node

--- src/evaluator/index.ts
+++ src/evaluator/index.ts
@@ -176,7 +176,8 @@ export default class Evaluator {
       return value;
     }
 
-    const exhaustiveCheck: never = node;
+    // TODO: uncomment
+    // const exhaustiveCheck: never = node;
   }
 }
 
