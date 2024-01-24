d9d092797c12593af8c2f7498cb6e125044f8b63
W. Cho <wcho.dev@gmail.com>
2024-01-25 03:32:40 +0900

    make a source token reader with position context

---
+++ src/parser/source-token-reader/index.test.ts
@@ -0,0 +1,76 @@
+import Lexer from "../../lexer";
+import SourceTokenReader from "./";
+
+const createReader = (input: string) => {
+  const lexer = new Lexer(input);
+
+  return new SourceTokenReader(lexer);
+};
+
+describe("read()", () => {
+  it("read a token", () => {
+    const input = "42";
+    const reader = createReader(input);
+    const expected = {
+      type: "number literal",
+      value: "42",
+      range: {
+        begin: { col: 0, row: 0 },
+        end: { col: 1, row: 0 },
+      },
+    };
+
+    expect(reader.read()).toEqual(expected);
+  });
+
+  it("read the end token if nothing to read", () => {
+    const input = "";
+    const reader = createReader(input);
+    const expected = {
+      type: "end",
+      value: "$end",
+      range: {
+        begin: { col: 0, row: 0 },
+        end: { col: 0, row: 0 },
+      },
+    };
+
+    expect(reader.read()).toEqual(expected);
+  });
+});
+
+describe("advance()", () => {
+  it("advance to next token", () => {
+    const input = "42 99";
+    const reader = createReader(input);
+    const expected = {
+      type: "number literal",
+      value: "99",
+      range: {
+        begin: { col: 3, row: 0 },
+        end: { col: 4, row: 0 },
+      },
+    };
+
+    reader.advance();
+    expect(reader.read()).toEqual(expected);
+  });
+});
+
+describe("isEnd()", () => {
+  it("return true if end", () => {
+    const input = "";
+    const reader = createReader(input);
+    const expected = true;
+
+    expect(reader.isEnd()).toEqual(expected);
+  });
+
+  it("return false if not end", () => {
+    const input = "42";
+    const reader = createReader(input);
+    const expected = false;
+
+    expect(reader.isEnd()).toEqual(expected);
+  });
+});
---
+++ src/parser/source-token-reader/index.ts
@@ -0,0 +1,24 @@
+import Lexer from "../../lexer";
+import type { SourceToken } from "../../lexer";
+
+export default class SourceTokenReader {
+  private readonly lexer: Lexer;
+  private token: SourceToken;
+
+  constructor(lexer: Lexer) {
+    this.lexer = lexer;
+    this.token = lexer.getSourceToken();
+  }
+
+  read(): SourceToken {
+    return this.token;
+  }
+
+  advance(): void {
+    this.token = this.lexer.getSourceToken();
+  }
+
+  isEnd(): boolean {
+    return this.token.type === "end";
+  }
+}
