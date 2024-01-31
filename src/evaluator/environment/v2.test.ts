a1ee8f3c9dab1ad9089afcb35504f00c6b4af9f0
Won-hee Cho <89635107+wcho21@users.noreply.github.com>
2024-01-31 19:34:37 +0900

    add evaluator with position context (#53)

---
+++ src/evaluator/environment/v2.test.ts
@@ -0,0 +1,52 @@
+import type { Value } from "../value";
+import Environment from "./v2";
+
+describe("set()", () => {
+  it("set name and value", () => {
+    const env = new Environment();
+    const varName = "foo";
+    const varValue = {} as Value;
+
+    expect(() => env.set(varName, varValue)).not.toThrow();
+  });
+});
+
+describe("get()", () => {
+  it("get value after setting the value", () => {
+    const env = new Environment();
+    const varName = "foo";
+    const varValue = {} as Value;
+
+    env.set(varName, varValue);
+
+    expect(env.get(varName)).toBe(varValue);
+  });
+
+  it("get null if not found", () => {
+    const env = new Environment();
+    const varNameNotSet = "foo";
+
+    expect(env.get(varNameNotSet)).toBe(null);
+  });
+});
+
+describe("linked environment", () => {
+  it("set super environment and get via sub environment", () => {
+    const varNameInSuper = "foo";
+    const varValueInSuper = {} as Value;
+
+    const superEnv = new Environment();
+    superEnv.set(varNameInSuper, varValueInSuper);
+    const subEnv = new Environment(superEnv);
+
+    expect(subEnv.get(varNameInSuper)).toBe(varValueInSuper);
+  });
+
+  it("get null if not found even in super environment", () => {
+    const superEnv = new Environment();
+    const subEnv = new Environment(superEnv);
+
+    const varNameSetNowhere = "foo";
+    expect(subEnv.get(varNameSetNowhere)).toBe(null);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import Lexer from "./lexer";
-import Parser from "./parser";
-import Evaluator, { Environment } from "./evaluator";
+import Parser from "./parser/v2";
+import Evaluator, { Environment } from "./evaluator/v2";
 
 export const execute = (input: string): string => {
   const lexer = new Lexer(input);
   const parser = new Parser(lexer);
-  const parsed = parser.parseProgram();
+  const parsed = parser.parseSource();
 
   const evaluator = new Evaluator();
   const environment = new Environment();
